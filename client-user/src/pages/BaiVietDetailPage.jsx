aa6d35e92cc0149ab189579caf38d541ec0ccc25
achehvinh <email_github_cua_ban@gmail.com>
2026-07-07 10:28:35 +0700

    Fix BaiVietDetailPage export

---
+++ client-user/src/pages/BaiVietDetailPage.jsx
@@ -0,0 +1,12 @@
+import './BaiVietDetailPage.css';
+
+function BaiVietDetailPage() {
+  return (
+    <div>
+      <h1>Chi tiết bài viết</h1>
+      <p>Trang đang được phát triển...</p>
+    </div>
+  );
+}
+
+export default BaiVietDetailPage;
