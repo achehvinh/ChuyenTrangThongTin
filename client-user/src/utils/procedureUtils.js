37a55d831025c8f6eaa2468ed21953c999e9a4a4
achehvinh <email_github_cua_ban@gmail.com>
2026-07-07 10:48:26 +0700

    Update project

--- client-user/src/utils/procedureUtils.js
+++ client-user/src/utils/procedureUtils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
+  import.meta.env.VITE_API_BASE_URL || "https://chuyen-trang-thong-tin-6os5.vercel.app/api/v1/bai-viet";
 
 export const api = axios.create({ baseURL: API_BASE_URL, timeout: 15000 });
 
